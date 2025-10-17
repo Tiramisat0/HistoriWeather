8a46af521f3fe82f2012b38fa2edac64ca43d18f
Satwik Toleti <toleti.sa@gmail.com>
2025-10-16 20:12:14 -0400

    updated webpage

--- apimanager.py
+++ apimanager.py
@@ -2,20 +2,20 @@ import requests
 
 base_url = "https://archive-api.open-meteo.com/v1/archive?"
 
-def get_day_temps(latitude, longitude, date):
+def get_hourly_temps(latitude, longitude, date):
     url = (
         f"{base_url}"
         f"latitude={latitude}&longitude={longitude}"
         f"&start_date={date}&end_date={date}"
-        f"&daily=temperature_2m_max,temperature_2m_min"
+        f"&hourly=temperature_2m"
         f"&timezone=auto"
     )
-    
+
     response = requests.get(url)
-    
+
     if response.status_code == 200:
         data = response.json()
-        temps = data.get("daily", {})
+        temps = data.get("hourly", {})
         print("Success!")
         return temps
     else:
@@ -23,4 +23,5 @@ def get_day_temps(latitude, longitude, date):
         return None
 
 # Example: Toronto on Aug 1, 2023
-print(get_day_temps(43.690685, -79.41174, "2023-08-01"))
+hourly_data = get_hourly_temps(43.690685, -79.41174, "2025-10-06")
+print(hourly_data)
